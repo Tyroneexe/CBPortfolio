301d26affa9014ddd34d1f1d4987e051564aec99
Tyroneexe <tvzbothma@gmail.com>
2023-12-22 12:30:23 +0200

    added colors

---
+++ lib/themes/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundClr = Color(0xFFFAF1F0);
+const Color primaryClr = Color(0xFFC64C45);
+const Color secondaryClr = Color(0xFFE0C59A);
+const Color accentClr = Color(0xFFD5C576);
