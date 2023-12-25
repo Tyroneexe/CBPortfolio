8c48e32a4076240e201433e56785093e0af74c7c
Tyroneexe <tvzbothma@gmail.com>
2023-12-25 16:50:27 +0200

    getting new colors

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,7 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
 import 'package:cobus/themes/colors.dart';
-import 'package:cobus/widgets/stat_container.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /*
@@ -21,111 +19,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroundClr,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 25, left: 50),
-                  child: GestureDetector(
-                    onTap: () {
-                      Get.to(() => const HomePage());
-                    },
-                    child: const Icon(
-                      Icons.person,
-                      size: 48,
-                      color: primaryClr,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 35, right: 40),
-                      child: GestureDetector(
-                        onTap: () {
-                          downloadFile("images/CV.pdf");
-                        },
-                        child: const Icon(
-                          Icons.download,
-                          size: 48,
-                          color: primaryClr,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 25, right: 50),
-                      child: GestureDetector(
-                        onTap: () => _launchUrl(),
-                        child: Image.asset(
-                          "images/LinkedIn.png",
-                          color: primaryClr,
-                          width: 44,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: primaryClr,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 1.5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Image.asset("images/Person.png")],
             ),
-            Stack(
-              children: [
-                Center(
-                  child: Image.asset(
-                    "images/Person.png",
-                  ),
-                ),
-              ],
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
             ),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width - 800,
-                height: 170,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.4),
-                      spreadRadius: 2,
-                      blurRadius: 4,
-                      offset: const Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 1,
-                    ),
-                    StatContainer(
-                      label: "Logistics",
-                      icon: Icons.cases_outlined,
-                    ),
-                    StatContainer(
-                      label: "BCom",
-                      icon: Icons.business,
-                    ),
-                    StatContainer(
-                      label: "Supply Chain",
-                      icon: Icons.local_shipping,
-                    ),
-                    SizedBox(
-                      width: 1,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-const Color backgroundClr = Color(0xFFFAF1F0);
-const Color primaryClr = Color(0xFFC64C45);
-const Color secondaryClr = Color(0xFFE0C59A);
-const Color accentClr = Color(0xFFD5C576);
+// 0xFF744A44
+// 0xFFFCE6E7
+// 0xFFAE6D71
+
+const Color backgroundClr = Color(0xFFFFF5F1);
+const Color primaryClr = Color(0xFF744A44);
+const Color secondaryClr = Color(0xFFAE6D71);
+const Color accentClr = Color(0xFFFCE6E7);
+
+// const Color backgroundClr = Color(0xFFFAF1F0);
+// const Color primaryClr = Color(0xFFC64C45);
+// const Color secondaryClr = Color(0xFFE0C59A);
+// const Color accentClr = Color(0xFFD5C576);
