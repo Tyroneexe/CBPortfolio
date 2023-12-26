359bd7160c452b82ff42946430fcda7af0da9858
Tyroneexe <tvzbothma@gmail.com>
2023-12-26 11:10:17 +0200

    added linkedin and resume icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
+import 'dart:html' as html;
 import 'package:cobus/themes/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -35,10 +35,143 @@ class _HomePageState extends State<HomePage> {
                   decoration: const BoxDecoration(
                     color: backgroundClr,
                   ),
-                  child: const Column(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [],
+                    children: [
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              refreshScreen();
+                            },
+                            child: const Row(
+                              children: [
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                Icon(
+                                  Icons.person,
+                                  size: 44,
+                                  color: primaryClr,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Cobus",
+                                  style: TextStyle(
+                                    fontFamily: "Roboto",
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 35,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 170,
+                          ),
+                          const Column(
+                            children: [
+                              SizedBox(
+                                height: 12,
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Home",
+                                    style: TextStyle(
+                                      fontFamily: "Roboto",
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w500,
+                                      color: primaryClr,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 40,
+                                  ),
+                                  Text(
+                                    "References",
+                                    style: TextStyle(
+                                      fontFamily: "Roboto",
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white54,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 40,
+                                  ),
+                                  Text(
+                                    "About",
+                                    style: TextStyle(
+                                      fontFamily: "Roboto",
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white54,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Expanded(
+                            child: SizedBox(
+                              width: 1,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              downloadFile("images/CV.pdf");
+                            },
+                            child: const Icon(
+                              Icons.sim_card_download_rounded,
+                              size: 38,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              _launchUrl();
+                            },
+                            child: Image.asset(
+                              "images/LinkedIn.png",
+                              color: Colors.white70,
+                              width: 36,
+                              height: 36,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          const Icon(
+                            Icons.menu_rounded,
+                            size: 42,
+                            color: Colors.white70,
+                          ),
+                          const SizedBox(
+                            width: 50,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: (MediaQuery.of(context).size.width / 2) - (512 / 2),
+                  top: 150,
+                  child: Image.asset(
+                    "images/Person.png",
                   ),
                 ),
                 Positioned(
@@ -75,8 +208,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   void downloadFile(url) {
-    AnchorElement anchorElement = AnchorElement(href: url);
+    html.AnchorElement anchorElement = html.AnchorElement(href: url);
     anchorElement.download = "Cobus Bothma Resume";
     anchorElement.click();
+  }
+
+  void refreshScreen() {
+    html.window.location.reload();
   }
 }
